5765e5438345d68432ae8978b2bd0d27cd6ba2b2
rim <rim123kaoutar@gmail.com>
2023-06-16 01:08:18 +0000

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,24 @@
 #include<stdio.h>
 
-int main(){
+/**
+ * main - Entry point
+ *
+ *prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19,
+ * followed by a new line, to the standard error
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
 	int i;
-	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	for(i=0;i<59;i++){
+	char quot[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	for (i = 0; i < 59; i++)
+
+	{
 		putchar(quote[i]);
+
 	}
-	return(1);
+	return (1);
 }
--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
 
-int main(){
+/**
+ * main - Entry point
+ *
+ * prints exactly "Programming is like building a multilingual puzzle
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
-}
+	return (0); }
