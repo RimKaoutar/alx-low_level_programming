1a19fd2e8110557d688b68c900b465535710929c
rim <rim123kaoutar@gmail.com>
2023-06-17 01:57:53 +0000

    done

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,30 +1,37 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-    int i = 48, j, k;
+	int i = 48, j, k;
 
-    while (i < 56)
-    {
-        j = i + 1;
-        k = i + 2;
-        while (j < 57 ){
-            
-            while (k < 58)
-            {
-                putchar(i);
-                putchar(j);
-                putchar(k++);
-                if (i == 55 && j == 56 && k == 58)
-                    break;
-                putchar(44);
-                putchar(32);
-                
-            }
-            j++;
-        
-        }
-        i++;
-    }
-    putchar(10);
+	while (i < 56)
+	{
+		j = i + 1;
+		k = i + 2;
+		while (j < 57)
+		{
+
+			while (k < 58)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k++);
+
+				if (i == 55 && j == 56 && k == 58)
+					break;
+
+				putchar(44);
+				putchar(32);
+			}
+		j++;
+		}
+	i++;
+	}
+	putchar(10);
 }
