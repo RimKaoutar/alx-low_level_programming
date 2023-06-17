fe7b3189d3638e3f0b0ac9c5ce93b37db98fc437
rim <rim123kaoutar@gmail.com>
2023-06-17 01:19:35 +0000

    done

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    char alpha[] = "abcdefghijklmnopqrstuvwxyz\n";
-    int i = 0;  
-    while (i < 27)
-    {
-        putchar(alpha[i++]);
-    } 
-    return (0);
+	char alpha[] = "abcdefghijklmnopqrstuvwxyz\n";
+	int i = 0;
+
+	while (i < 27)
+		putchar(alpha[i++]);
+
+	return (0);
 }
