7eb55fd0b9cb9d9f0e8c16a2b9bfae5c4c314e40
rim <rim123kaoutar@gmail.com>
2023-06-17 01:34:43 +0000

    done

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-    int i = 48;  
-    while (i < 103)
-    {
-	if (i > 57 && i++ < 97)
-	    continue;
-        putchar(i++);
-    }
-    putchar('\n'); 
-    return (0);
+	int i = 48;
+
+	while (i < 103)
+	{
+		if (i > 57 && i++ < 97)
+			continue;
+		putchar(i++);
+	}
+	putchar('\n');
+	return (0);
 }
