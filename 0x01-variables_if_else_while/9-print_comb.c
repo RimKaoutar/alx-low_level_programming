c7fd553e1dca39bc3f9272b35e1fd78e45fd1f89
rim <rim123kaoutar@gmail.com>
2023-06-17 01:40:34 +0000

    done

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-    int i = 48;  
-    while (i < 58)
-    {
-        putchar(i);
-	if (i++ == 57)
-	    continue;
-	putchar(44);
-	putchar(32);
-    }
-    putchar(10); 
-    return (0);
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		if (i++ == 57)
+			continue;
+		putchar(44);
+		putchar(32);
+	}
+	putchar(10);
+	return (0);
 }
