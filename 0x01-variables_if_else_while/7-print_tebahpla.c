86f92ada8f5bc07174af022a01f5430aa6e0549e
rim <rim123kaoutar@gmail.com>
2023-06-17 01:33:04 +0000

    done

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-    int i = 122;  
-    while (i > 96)
-    {
-        putchar(i--);
-    }
-    putchar('\n'); 
-    return (0);
+	int i = 122;
+
+	while (i > 96)
+	{
+		putchar(i--);
+	}
+	putchar('\n');
+	return (0);
 }
