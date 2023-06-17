6b7e01e6e3923234c99cdf76201c53e3ebd385b9
rim <rim123kaoutar@gmail.com>
2023-06-17 01:31:44 +0000

    done

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-    int i = 48;  
-    while (i < 58)
-    {
-        putchar(i++);
-    }
-    putchar(10); 
-    return (0);
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i++);
+	}
+	putchar(10);
+	return (0);
 }
