7eb7b58d06a7d8d227ed687008da2c1e93754089
rim <rim123kaoutar@gmail.com>
2023-06-17 01:29:27 +0000

    done

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-    char i = 48;  
-    while (i < 58)
-    {
-        putchar(i++);
-    }
-    putchar('\n'); 
-    return (0);
+	char i = 48;
+
+	while (i < 58)
+	{
+		putchar(i++);
+	}
+	putchar('\n');
+	return (0);
 }
