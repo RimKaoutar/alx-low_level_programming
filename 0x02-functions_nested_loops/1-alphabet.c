193409980427436de49ef84e4b3dd1a1718fbae9
rim <rim123kaoutar@gmail.com>
2023-06-21 01:47:13 +0000

    done

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,21 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_alphabet - function
+ *
+ * Description: prints the alphabet, in lowercase, followed by a new line.
  *
  * Return: Always 0.
  */
-int main(void)
+void print_alphabet(void)
 {
-    print_alphabet();
-    return (0);
+	char alpha[] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		_putchar(alpha[i]);
+	}
+	_putchar("\n");
+	return (0);
 }
