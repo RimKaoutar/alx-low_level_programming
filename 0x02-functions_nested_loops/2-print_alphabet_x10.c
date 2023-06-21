92393e0155f70652ef326e107057c98033c6fc74
rim <rim123kaoutar@gmail.com>
2023-06-21 01:59:19 +0000

    done

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -15,7 +15,7 @@ void print_alphabet_x10(void)
 	char alpha[] = "abcdefghijklmnopqrstuvwxyz";
 	int i, j;
 
-	for (j = 0; i < 10; j++)
+	for (j = 0; j < 10; j++)
 	{
 		for (i = 0; i < 26; i++)
 		{
