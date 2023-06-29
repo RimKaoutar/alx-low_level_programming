5c713d769f282d839c724759a86a934ec8e69a7b
rim <rim123kaoutar@gmail.com>
2023-06-29 20:27:02 +0000

    done

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncpy - A function that copies a string.
+ *
+ * @dest: pointer to destination input buffer
+ * @src: pointer to source input buffer
+ * @n: bytes of @src
+ *
+ * Return: @dest
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int len = 0;
+
+	while (len < n)
+	{
+		dest[len] = src[len];
+
+		if (src[len] == '\0')
+			break;
+
+		len++;
+	}
+	return (dest);
+}
