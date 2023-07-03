dccf2cd64f8b232d413768d7218cbad3afff23cc
rim <rim123kaoutar@gmail.com>
2023-07-03 19:50:05 +0000

    done

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chess board
+ *
+ * @a: input pointer
+ *
+ * Return: return nothing
+*/
+
+void print_chessboard(char (*a)[8])
+{
+	int i = 0, j;
+
+	while (i < 8)
+	{
+		j = 0;
+		while (j < 8)
+		{
+			_putchar(a[i][j]);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
