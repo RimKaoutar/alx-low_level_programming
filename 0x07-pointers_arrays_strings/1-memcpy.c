79dc4e9247291bc07b297da5aaac2247253fffe0
rim <rim123kaoutar@gmail.com>
2023-07-03 18:11:06 +0000

    done

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,10 +12,10 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-unsigned int i = 0;
+	unsigned int i = 0;
 
-while (i < n)
-dest[i] = src[i];
+	while (i < n)
+		dest[i] = src[i];
 
-return (dest);
+	return (dest);
 }
