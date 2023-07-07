502841dce1f26197a5fda26ef65cb0b8cef72c7d
rim <rim123kaoutar@gmail.com>
2023-07-07 16:14:06 +0000

    done

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of a program and a new line.
+ * @argc: argument count, holds the number of arguments passed.
+ * @argv: argument vector, array pointer that holds the arguments passed.
+ * Return: 0 if success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", *argv[0]);
+	return (0);
+}
