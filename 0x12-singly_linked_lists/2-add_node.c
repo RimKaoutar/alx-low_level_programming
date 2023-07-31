804820a6c2be980e752513b32af2ca59fcd74e43
rim <rim123kaoutar@gmail.com>
2023-07-31 01:26:05 +0100

    Add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -15,12 +15,17 @@ list_t *add_node(list_t **head, const char *str)
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+	if (new->str == NULL)
 	{
+		free(new);
 		return (NULL);
 	}
-	new->str = strdup(str);
 	while (str++)
 		len++;
+
 	new->len = len;
 	new->next = *head;
 
