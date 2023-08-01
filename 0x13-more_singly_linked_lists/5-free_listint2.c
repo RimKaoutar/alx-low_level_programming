928405d3fc1170a93df4ba032ab02141432d1a18
rim <rim123kaoutar@gmail.com>
2023-08-01 23:56:28 +0100

    Free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint2 - that frees a listint_t list.
+ * @head: is the input of the list
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *gone;
+
+	while (*head)
+	{
+		gone = *head;
+		*head = (*head)->next;
+		free(gone);
+	}
+	*head = NULL;
+
+}
