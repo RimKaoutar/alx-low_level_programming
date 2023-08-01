d887f133da54d2ffd4a8081feb66748a8cac7e59
rim <rim123kaoutar@gmail.com>
2023-08-02 00:50:05 +0100

    Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	tmp = *head;
 	while (tmp)
 	{
-		if (i - 1 == idx)
+		if (i == idx - 1)
 		{
 			new->next = tmp->next;
 			tmp->next = new;
