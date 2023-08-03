0d0cfa425cb5f6fab01507dba2234c9e2e197603
rim <rim123kaoutar@gmail.com>
2023-08-03 21:17:07 +0100

    done

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* print_binary - that prints the binary representation of a number.
+* @n: is the number of the Input
+*/
+void print_binary(unsigned long int n)
+{
+	if (n > 0)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
+	
+}
