0bc2904690afba6436cf28e340a1442e363fdbec
rim <rim123kaoutar@gmail.com>
2023-08-03 01:22:20 +0100

    Print (safe version)

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,37 +3,28 @@
  * print_listint_safe - that prints a listint_t linked list.
  * @head: the nput list
  * Return: the number nodes
- */
+*/
 
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t i;
-	const listint_t *slow, *fast;
+	long int j;
 
 	if (head == NULL)
-		return (0);
-
+		exit(98);
 	i = 0;
-	slow = head;
-	fast = head;
-
-	while (fast && fast->next)
-	{
-		slow = slow->next;
-		fast = fast->next->next;
-
-		if (slow == fast)
-		{
-			exit(98);
-		}
-	}
-
 	while (head)
 	{
-		printf("-> [%p] %d\n", (void *)head, head->n);
-		head = head->next;
+		j = head - head->next;
 		i++;
+		printf("[%p] %d\n", (void *)head, head->n);
+		if (j <= 0)
+		{
+			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+			break;
+		}
+		else
+			head = head->next;
 	}
-
 	return (i);
 }
