6b5bd395e917e9abd852d0d8d02927813e150a06
rim <rim123kaoutar@gmail.com>
2023-08-08 02:54:15 +0100

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 	for (i = 0; text_content[i]; i++)
 		;
 	n = write(fd, text_content, i);
-	if (!n)
+	if (n == -1)
 		return (-1);
 	close(fd);
 	return (1);
