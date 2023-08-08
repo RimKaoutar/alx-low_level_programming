052c5079265f973eabce3c545cc7b2831db8644e
rim <rim123kaoutar@gmail.com>
2023-08-08 02:48:27 +0100

    Under the snow

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: filename.
+ * @text_content: content writed in the file.
+ *
+ * Return: 1 if it success. -1 if it fails.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, i, n;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (!fd)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	for (i = 0; text_content[i]; i++)
+		;
+	n = write(fd, text_content, i);
+	if (!n)
+		return (-1);
+	close(fd);
+	return (1);
+}
