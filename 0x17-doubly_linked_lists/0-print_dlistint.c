92c8b987bc36a3a47b8245f73de5d0ddc61e8942
RimKaoutar <rim.kaoutar-etu@etu.univh2c.ma>
2023-09-14 22:07:44 +0100

    Print list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,8 +9,12 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	int cpt = 0;
 
+	while (h == NULL)
+		return (cpt);
+
 	while (h->prev != NULL)
 		h = h->prev;
+
 	while (h)
 	{
 		printf("%d\n", h->n);
