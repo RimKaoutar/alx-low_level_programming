5948f6576441b4d2250b33920535043dda04d2d9
RimKaoutar <rim.kaoutar-etu@etu.univh2c.ma>
2023-09-15 02:33:29 +0100

    Insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -8,7 +8,7 @@
  * @n: Integer for new node's data field
  *
  * Return: Pointer to new node or NULL on failure
-*/
+ */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new;
@@ -17,20 +17,25 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (*h == NULL)
 		return (NULL);
-	current = *h;
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
+	if (idx == 0)
+		new = add_dnodeint(h, n);
+	else
+	{
+		current = *h;
+		new = malloc(sizeof(dlistint_t));
+		if (new == NULL)
+			return (NULL);
+		new->n = n;
 
-	while (i++ != idx && current)
-		current = current->next;
+		while (i++ != idx && current)
+			current = current->next;
 
-	if (i != idx)
-		return (NULL);
-	new->prev = current->prev;
-	new->next = current;
-	new->prev->next = new;
-	current->prev = new;
-	return (new);
+		if (i != idx)
+			return (NULL);
+		new->prev = current->prev;
+		new->next = current;
+		new->prev->next = new;
+		current->prev = new;
+		return (new);
+	}
 }
