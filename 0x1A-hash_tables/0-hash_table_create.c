6e9647ad1669a57e71734a8d8748b9a2d919f808
RimKaoutar <rim.kaoutar-etu@etu.univh2c.ma>
2023-10-07 02:37:40 +0100

    >>> ht = {}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,11 +12,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	if (size < 1)
 		return (NULL);
+
 	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
 		return (NULL);
+
 	new_table->size = size;
-	new_table->array = malloc(sizeof(hash_table_t));
+	new_table->array = malloc(sizeof(hash_table_t) * size);
+
 	if (new_table->array == NULL)
 	{
 		free(new_table);
